2e1f8587ceb65fa38b53e5136cb0b9b814d9e333
v3ry <v3ryfr@gmail.com>
2023-01-02 17:03:24 +0100

    angular 11

--- src/app/cocktail-list/cocktail-list.component.ts
+++ src/app/cocktail-list/cocktail-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Cocktail } from '../cocktail';
 import { CocktailServiceService } from '../cocktail-service.service';
 
@@ -7,11 +7,30 @@ import { CocktailServiceService } from '../cocktail-service.service';
   templateUrl: './cocktail-list.component.html',
   styleUrls: ['./cocktail-list.component.css'],
 })
-export class CocktailListComponent {
+export class CocktailListComponent implements OnInit {
   cocktails: Cocktail[] = [];
-  constructor(private cocktailServiceService: CocktailServiceService) {
-    this.cocktailServiceService
-      .getCocktails()
-      .subscribe((cocktails) => (this.cocktails = cocktails));
+
+  constructor(public cocktailService: CocktailServiceService) {}
+
+  ngOnInit(): void {
+    this.cocktailService.getCocktails().subscribe((cocktailsFromJsonFile) => {
+      console.log(cocktailsFromJsonFile);
+      //       // Step 1. Get all the object keys.
+      // let spaghettiProperties = Object.keys(cocktailsFromJsonFile);
+
+      // // Step 2. Create an empty array.
+      // let neededArray = [];
+
+      // // Step 3. Iterate throw all keys.
+      // let i = 0;
+      // for (prop of spaghettiProperties ) {
+      //     neededArray.push(cocktailsFromJsonFile[prop]);
+      //     neededArray[i].['name'] = prop;
+      //     i++;
+      // }
+      // console.log(neededArray)
+      this.cocktails = cocktailsFromJsonFile;
+      // console.log(JSON.parse(this.cocktails.toString()));
+    });
   }
 }
--- src/app/cocktail-service.service.ts
+++ src/app/cocktail-service.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, Inject, Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Cocktail } from './cocktail';
 import { COCKTAILS } from './mock-cocktails';
@@ -7,21 +8,9 @@ import { COCKTAILS } from './mock-cocktails';
   providedIn: 'root',
 })
 export class CocktailServiceService {
-  constructor() {}
+  constructor(public http: HttpClient) {}
 
   getCocktails(): Observable<Cocktail[]> {
-    const heroes = of(COCKTAILS);
-    console.log(heroes);
-    return heroes;
+    return this.http.get<Cocktail[]>('assets/cocktails.json');
   }
-
-  // public getCocktails(): Array<CocktailServiceService> {
-  //   // const cocktail1 = new CocktailServiceService('Titi', 1, 'blabla');
-  //   // const cocktail2 = new CocktailServiceService('Titi', 1, 'blabla');
-  //   // const cocktail3 = new CocktailServiceService('Titi', 1, 'blabla');
-  //   const list = [];
-  //   // list.push(cocktail1);
-  //   // list.push(cocktail2);
-  //   // list.push(cocktail3);
-  //   return list;
 }
